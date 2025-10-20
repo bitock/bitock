d9e759fb582780258090cd4ea5aa0cdbcd174d69
bitock <99618654+bitock@users.noreply.github.com>
2025-10-20 18:56:55 +0900

    Update clone.sh

--- .sh/clone.sh
+++ .sh/clone.sh
@@ -1,7 +1,9 @@
 
 git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/bitock bitock
+git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/website website
+git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/bookmark bookmark
 git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/technology technology
 git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/terminology terminology
-git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/company company
-git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/micronano micronano
 git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/event event
+git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/micronano micronano
+git clone --origin github --branch master --depth 1 --single-branch -- github:bitock/company company
